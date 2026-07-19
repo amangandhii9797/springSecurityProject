ccfe0252fa76d00020d4e02768b4a77c837f799d
amangandhii9797 <amangandhii9797@gmail..com>
2026-07-19 18:28:50 +0530

    spring security using in memory database for storing user details

--- src/main/java/org/example/hospitalManagement_SpringSecurity/config/AppConfig.java
+++ src/main/java/org/example/hospitalManagement_SpringSecurity/config/AppConfig.java
@@ -3,6 +3,8 @@ package org.example.hospitalManagement_SpringSecurity.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class AppConfig {
@@ -10,5 +12,12 @@ public class AppConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+
+    // This is required when creating new user , as we can not store there passwords in plain text
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
