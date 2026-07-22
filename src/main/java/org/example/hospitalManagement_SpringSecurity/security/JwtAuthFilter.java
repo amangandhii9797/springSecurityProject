7b45385ccf53b075df7b0922ab29c092ad64031c
amangandhii9797 <amangandhii9797@gmail..com>
2026-07-22 23:43:22 +0530

    added error handling

--- src/main/java/org/example/hospitalManagement_SpringSecurity/security/JwtAuthFilter.java
+++ src/main/java/org/example/hospitalManagement_SpringSecurity/security/JwtAuthFilter.java
@@ -12,6 +12,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.io.IOException;
 
@@ -24,29 +25,37 @@ public class JwtAuthFilter  extends OncePerRequestFilter {
 
     private  final UserRepository userRepository;
     private final AuthUtil authUtil;
+    private final HandlerExceptionResolver  handlerExceptionResolver;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        log.info("incoming request: {}", request.getRequestURI());
-        final String requestTokenHeader = request.getHeader("Authorization");
-        if (requestTokenHeader == null || !requestTokenHeader.startsWith("Bearer ")) {
-            log.warn("JWT Token does not begin with Bearer String");
+        try {
+
+
+            log.info("incoming request: {}", request.getRequestURI());
+            final String requestTokenHeader = request.getHeader("Authorization");
+            if (requestTokenHeader == null || !requestTokenHeader.startsWith("Bearer ")) {
+                log.warn("JWT Token does not begin with Bearer String");
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            String token = requestTokenHeader.substring(7).trim();
+            String username = authUtil.getUsernameFromToken(token);
+
+
+            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                var user = userRepository.findByUsername(username).orElseThrow(() -> new RuntimeException("User not found"));
+                var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+
             filterChain.doFilter(request, response);
-            return;
+
+        } catch (Exception ex) {
+            handlerExceptionResolver.resolveException(request,response,null,ex);
         }
-
-        String token = requestTokenHeader.substring(7).trim();
-        String username = authUtil.getUsernameFromToken(token);
-
-
-        if(username != null && SecurityContextHolder.getContext().getAuthentication() == null){
-            var user = userRepository.findByUsername(username).orElseThrow(() -> new RuntimeException("User not found"));
-            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-        }
-
-        filterChain.doFilter(request, response);
 
     }
 }
